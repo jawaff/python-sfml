54c7273849904747a09a2f36b8010b8be36e7ed3
jawaff <jacqueswafel@gmail.com>
2018-10-18 16:53:57 -0700

    git status

--- scripts/windows_env_setup.ps1
+++ scripts/windows_env_setup.ps1
@@ -1,6 +1,12 @@
 choco install python
 
+echo "C:"
+ls C:\
+echo "C:\Users"
+ls C:\Users
+
 # TODO Where does Python get installed to? The PATH doesn't get updated.
+# $ENV:PATH = $ENV:PATH + ";C:\Users\Jake\AppData\Local\Programs\Python\Python37"
 
 # Refreshes environment variables updated by choco.
 refreshenv
--- setup.py
+++ setup.py
@@ -70,11 +70,11 @@ class CythonBuildExt(build_ext):
 
 modules = ['system', 'window', 'graphics', 'audio', 'network']
 
-extension = lambda name, files, libs,  lib_dir: Extension(
+extension = lambda name, files, libs: Extension(
     name='sfml.' + name,
     sources= [os.path.join('src', 'sfml', name, filename) for filename in files],
-    include_dirs=[os.path.join('include', 'Includes')],
-    library_dirs=[lib_dir, os.path.join('extlibs', 'libs-msvc-universal', arch)] if sys.hexversion >= 0x03050000 else [],
+    include_dirs=[os.path.join('include', 'Includes'), os.path.join('extlibs', 'SFML-2.3.2', 'include')],
+    library_dirs=[os.path.join('extlibs', 'SFML-2.3.2', 'lib'), os.path.join('extlibs', 'libs-msvc-universal', arch)] if sys.hexversion >= 0x03050000 else [os.path.join('extlibs', 'SFML-2.3.2', 'lib')],
     language='c++',
     libraries=libs,
     define_macros=[('SFML_STATIC', '1')] if platform.system() == 'Windows' else [])
@@ -85,52 +85,39 @@ if platform.system() == 'Windows':
     graphics_libs    = ['user32', 'advapi32', 'winmm', 'sfml-system-s', 'gdi32', 'opengl32', 'sfml-window-s', 'freetype', 'jpeg', 'sfml-graphics-s']
     audio_libs       = ['winmm', 'sfml-system-s', 'flac', 'vorbisenc', 'vorbisfile', 'vorbis', 'ogg', 'openal32', 'sfml-audio-s']
     network_libs     = ['ws2_32', 'sfml-system-s', 'sfml-network-s']
-    sfml_lib_dir     = os.path.join('extlibs', 'SFML-2.3.2', 'bin')
 else:
     system_libs      = ['sfml-system']
     window_libs      = ['sfml-system', 'sfml-window']
     graphics_libs    = ['sfml-system', 'sfml-window', 'sfml-graphics']
     audio_libs       = ['sfml-system', 'sfml-audio']
     network_libs     = ['sfml-system', 'sfml-network']
-    sfml_lib_dir     = os.path.join('extlibs', 'SFML-2.3.2', 'lib')
-
 
 system = extension(
     'system',
     ['system.pyx', 'error.cpp', 'hacks.cpp', 'NumericObject.cpp'],
-    system_libs,
-    sfml_lib_dir)
+    system_libs)
 
 window = extension(
     'window',
     ['window.pyx', 'DerivableWindow.cpp'],
-    window_libs,
-    sfml_lib_dir)
+    window_libs)
 
 graphics = extension(
     'graphics',
     ['graphics.pyx', 'DerivableRenderWindow.cpp', 'DerivableDrawable.cpp', 'NumericObject.cpp'],
-    graphics_libs,
-    sfml_lib_dir)
+    graphics_libs)
 
 audio = extension(
     'audio',
     ['audio.pyx', 'DerivableSoundRecorder.cpp', 'DerivableSoundStream.cpp'],
-    audio_libs,
-    sfml_lib_dir)
+    audio_libs)
 
 network = extension(
     'network',
     ['network.pyx'],
-    network_libs,
-    sfml_lib_dir)
+    network_libs)
 
 major, minor, _, _ , _ = sys.version_info
-
-data_files = []
-#if platform.system() == 'Windows':
-#    dlls = [("Lib\\site-packages\\sfml", glob('extlibs\\SFML-2.3.2\\bin'))]
-#    data_files += dlls
 
 with open('README.md', 'r') as f:
     long_description = f.read()
@@ -147,7 +134,6 @@ kwargs = dict(
             ext_modules=ext_modules,
             package_dir={'': 'src'},
             packages=['sfml'],
-            data_files=data_files,
             version='2.3.2.dev1',
             description='Python bindings for SFML',
             long_description=long_description,
