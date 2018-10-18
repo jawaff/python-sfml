ca278387ed97073a7cb6ce835c185535dd8726c4
jawaff <jacqueswafel@gmail.com>
2018-10-17 17:01:42 -0700

    Fixed a typo with the environment variables.

--- scripts/sfml_install.sh
+++ scripts/sfml_install.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-# Retrieves the directory of this script.
-CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
 wget https://www.sfml-dev.org/files/SFML-2.3.2-linux-gcc-64-bit.tar.gz
 
 # SFML-2.3.2 is the directory that is placed into extLibs/.
 # The SFML dir contains an include/, lib/ and share/ directory.
 tar -xvzf SFML-2.3.2-linux-gcc-64-bit.tar.gz -C extlibs/
 
+# Retrieves the directory of this script.
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 # Compile time environment variables for gcc.
-export CPPFLAGS="-I$CUR_DIR/extlibs/SFML-2.3.2/lib"
+export CPPFLAGS="-I$CUR_DIR/extlibs/SFML-2.3.2/include"
 export LIBRARY_PATH="$CUR_DIR/extlibs/SFML-2.3.2/lib"
+
+echo $CPPFLAGS
+echo $LIBRARY_PATH
