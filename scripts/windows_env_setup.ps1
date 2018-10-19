0bc77e791a05623bf11967f83c784e9dcd28ad01
jawaff <jacqueswafel@gmail.com>
2018-10-18 19:50:00 -0700

    Fixed the variables in the Windows env script.

--- scripts/windows_env_setup.ps1
+++ scripts/windows_env_setup.ps1
@@ -1,12 +1,11 @@
 & choco install python
 
 $PYTHON_HOME="C:\Pyton37"
-$PYTHON_EXE="$PYTHON_HOME\python.exe"
-$SCRIPTS_DIR="$PSScriptRoot"
+$PYTHON_EXE=$PYTHON_HOME\python.exe
+$SCRIPTS_DIR=$PSScriptRoot
 
-$PYTHON_EXE --version
-
-$PYTHON_EXE $SCRIPTS_DIR\get-pip.py
+& $PYTHON_EXE --version
+& $PYTHON_EXE "$SCRIPTS_DIR\get-pip.py"
 
 # Refreshes environment variables updated by choco.
 refreshenv
