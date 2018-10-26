e4970c7f50447437fe2d53961316fe2d1d25315c
jawaff <jacqueswafel@gmail.com>
2018-10-25 20:49:19 -0700

    Add the necessary environment variables for the manylinux pysfml build on a CentOS Docker image.

--- scripts/build_linux_wheels.sh
+++ scripts/build_linux_wheels.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e -x
 
+# Argumets
+CUR_OS="$1"
+SFML_VERSION="$2"
+
 # Install a system package required by our library
 yum install -y atlas-devel
 
@@ -9,6 +13,13 @@ for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w dist/
 done
+
+# Retrieves the directory of this script.
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo Script Dir: $SCRIPT_DIR
+
+. $SCRIPT_DIR/env_setup.sh $CUR_OS $SFML_VERSION
 
 # Bundle external shared libraries into the wheels
 for whl in dist/*.whl; do
